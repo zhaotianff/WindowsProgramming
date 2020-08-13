ee14f96473213bbd4a5ad503baf861caed914d42
zhaotianff <zhaotianff@DESKTOP-VPOI8TT>
2020-08-13 18:14:17 +0800

    添加注入示例代码

---
+++ Hook/Hook.h
@@ -0,0 +1,32 @@
+﻿
+// Hook.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CHookApp:
+// 有关此类的实现，请参阅 Hook.cpp
+//
+
+class CHookApp : public CWinApp
+{
+public:
+	CHookApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CHookApp theApp;
---
+++ Hook/HookDlg.h
@@ -0,0 +1,36 @@
+﻿
+// HookDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CHookDlg 对话框
+class CHookDlg : public CDialogEx
+{
+// 构造
+public:
+	CHookDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_HOOK_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+};
