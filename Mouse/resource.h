bdee4314806ab3d5a348c0d1aa37f2fc5d7f6bfa
zhaotianff <zhaotianff@163.com>
2020-09-09 10:49:46 +0800

    更新README.md

--- Mouse/resource.h
+++ Mouse/resource.h
@@ -9,11 +9,12 @@
 #define IDR_MAINFRAME                   128
 #define IDC_BUTTON1                     1000
 #define IDC_BUTTON2                     1001
-#define IDC_EDIT1                       1002
 #define IDC_EDIT2                       1003
 #define IDC_EDIT3                       1004
 #define IDC_EDIT4                       1005
 #define IDC_BUTTON3                     1006
+#define IDC_BUTTON4                     1007
+#define IDC_BUTTON5                     1008
 
 // Next default values for new objects
 // 
@@ -21,7 +22,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
--- README.md
+++ README.md
@@ -8,15 +8,28 @@ Windows programming sample code
 
 ## 代码列表
 * [磁盘](https://github.com/zhaotianff/WindowsProgramming/tree/master/Disk)
+  * 获取磁盘卷标名
+  * 获取磁盘空间
+  * 格式化磁盘
+  * 监控磁盘文件变化 
+  * 弹出光驱
 * [桌面](https://github.com/zhaotianff/WindowsProgramming/tree/master/Desktop)
+  * 隐藏/显示桌面
+  * 隐藏/显示开始菜单
+  * 隐藏/显示任务栏
+  * 隐藏/显示时钟
+  * 获取桌面图标列表
 * [剪贴板](https://github.com/zhaotianff/WindowsProgramming/tree/master/ClipBoard)
-  * 剪贴板操作
+  * 设置内容到剪贴板
+  * 读取剪贴板内容
+  * 列举剪贴板中的数据类型
 * [键盘](https://github.com/zhaotianff/WindowsProgramming/tree/master/KeyBoard)
   * 获取按键状态
   * 模拟键盘按下
 * [鼠标](https://github.com/zhaotianff/WindowsProgramming/tree/master/Mouse)
   * 交换鼠标左右键
-  * 
+  * 模拟鼠标操作
+  * 获取鼠标下窗体信息
 * [钩子](https://github.com/zhaotianff/WindowsProgramming/tree/master/Hook)
 
 ## Licenses
