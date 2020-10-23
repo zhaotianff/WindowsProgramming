e1855411a7a5031111997c114dda71b9bf3a8dfa
zhaotianff <zhaotianff@163.com>
2020-10-23 09:22:33 +0800

    更新消息示例代码

---
+++ Message/Message.h
@@ -0,0 +1,32 @@
+﻿
+// Message.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMessageApp:
+// 有关此类的实现，请参阅 Message.cpp
+//
+
+class CMessageApp : public CWinApp
+{
+public:
+	CMessageApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMessageApp theApp;
