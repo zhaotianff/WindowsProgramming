616a62fa67360c87f97eab294d54363239184b13
zhaotianff <zhaotianff@163.com>
2021-03-01 21:36:19 +0800

    增加字符串和字符文本示例代码

---
+++ StringLiteral/StringLiteral.cpp
@@ -0,0 +1,48 @@
+﻿// StringLiteral.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<string>
+
+using namespace std::string_literals;
+
+int main()
+{
+    // Character literals
+    auto c0 = 'A'; // char
+    auto c1 = u8'A'; // char
+    auto c2 = L'A'; // wchar_t
+    auto c3 = u'A'; // char16_t
+    auto c4 = U'A'; // char32_t
+
+    // Multicharacter literals
+    auto m0 = 'abcd'; // int, value 0x61626364
+
+    // String literals
+    auto s0 = "hello"; // const char*
+    auto s1 = u8"hello"; // const char*, encoded as UTF-8
+    auto s2 = L"hello"; // const wchar_t*
+    auto s3 = u"hello"; // const char16_t*, encoded as UTF-16
+    auto s4 = U"hello"; // const char32_t*, encoded as UTF-32
+
+    // Raw string literals containing unescaped \ and "
+    auto R0 = R"("Hello \ world")"; // const char*
+    auto R1 = u8R"("Hello \ world")"; // const char*, encoded as UTF-8
+    auto R2 = LR"("Hello \ world")"; // const wchar_t*
+    auto R3 = uR"("Hello \ world")"; // const char16_t*, encoded as UTF-16
+    auto R4 = UR"("Hello \ world")"; // const char32_t*, encoded as UTF-32
+
+    // Combining string literals with standard s-suffix
+    auto S0 = "hello"s; // std::string
+    auto S1 = u8"hello"s; // std::string
+    auto S2 = L"hello"s; // std::wstring
+    auto S3 = u"hello"s; // std::u16string
+    auto S4 = U"hello"s; // std::u32string
+
+    // Combining raw string literals with standard s-suffix
+    auto S5 = R"("Hello \ world")"s; // std::string from a raw const char*
+    auto S6 = u8R"("Hello \ world")"s; // std::string from a raw const char*, encoded as UTF-8
+    auto S7 = LR"("Hello \ world")"s; // std::wstring from a raw const wchar_t*
+    auto S8 = uR"("Hello \ world")"s; // std::u16string from a raw const char16_t*, encoded as UTF-16
+    auto S9 = UR"("Hello \ world")"s; // std::u32string from a raw const char32_t*, encoded as UTF-32
+}
