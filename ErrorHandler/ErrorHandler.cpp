cfb2fe27b98bf3e7edecbeb811c649d831a20c5e
zhaotianff <zhaotianff@163.com>
2021-03-23 22:03:21 +0800

    增加错误处理示例代码

---
+++ ErrorHandler/ErrorHandler.cpp
@@ -0,0 +1,61 @@
+﻿// ErrorHandler.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<Windows.h>
+
+using namespace std;
+
+BOOL ShowErrorInfo();
+int main()
+{
+    //当Windows 函数检测到错误时，会使用一种名为“线程本地存储区”（thread-local storeage）的机制将相应的错误代码与
+    //调用线程关联到一起
+    //这种机制使不同的线程能独立运行，不会出现相互干扰对方的错误代码的情况
+    //使用GetLastError()函数来获取函数执行的错误
+    //需要注意的时，由于是所有的线程错误都存储在一个位置，所以当发生错误后，要马上调用GetLastError()函数来获取错误码，否则结果可能不对了
+
+
+    //使用监视窗口的$err,hr可以查看错误代码具体代表什么意思
+
+    ShowErrorInfo();
+
+}
+
+BOOL ShowErrorInfo()
+{
+    DWORD errorCode = 0x02;
+    PCTSTR lpszErrorInfo = NULL;
+
+    HLOCAL hlocal = NULL;  //存储错误信息字符串的缓冲区
+    DWORD systemLocale = MAKELANGID(LANG_NEUTRAL,SUBLANG_NEUTRAL); //使用操作系统默认的语言
+
+    BOOL fOK = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+        NULL,errorCode,systemLocale,
+        (PTSTR)&hlocal, 0, NULL);
+
+    if (!fOK)
+    {
+        //网络相关的错误
+        HMODULE hDll = LoadLibraryEx(L"netmsg.dll", NULL, DONT_RESOLVE_DLL_REFERENCES);
+
+        if (hDll)
+        {
+            fOK = FormatMessage(FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+                hDll, errorCode, systemLocale,
+                (PTSTR)&hlocal, 0, NULL);
+        }
+    }
+    
+    if (fOK && (hlocal != NULL))
+    {
+        lpszErrorInfo = (PCTSTR)LocalLock(hlocal);
+        LocalFree(hlocal);
+        return TRUE;
+    }
+    else
+    {
+        lpszErrorInfo = L"";
+        return FALSE;
+    }
+}
