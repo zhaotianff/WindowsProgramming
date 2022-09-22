8ee822ac9edf6fdb3e91e11b30276fc40da1e476
zhaotianff <zhaotianff@163.com>
2022-09-22 13:59:40 +0800

    add create com obj code

---
+++ COM/COM.cpp
@@ -0,0 +1,34 @@
+﻿// COM.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<Windows.h>
+#include<ShObjIdl.h>
+#include<ShObjIdl_core.h>
+
+VOID CreateComObj()
+{
+    IVirtualDesktopManager* pDesktopManager = NULL;
+    HRESULT hr = CoInitialize(NULL);
+
+    if (SUCCEEDED(hr))
+    {
+        hr = CoCreateInstance(
+            CLSID_VirtualDesktopManager,
+            NULL,
+            CLSCTX_INPROC_SERVER,
+            IID_IVirtualDesktopManager, (void**)&pDesktopManager);
+
+        if (SUCCEEDED(hr))
+        {
+            pDesktopManager->Release();
+        }
+    }
+
+    CoUninitialize();
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
