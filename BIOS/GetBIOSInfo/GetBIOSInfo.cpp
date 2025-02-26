ae43eee2e7293b04d3f29e990a1ed34cda222498
zhaotianff <zhaotianff@163.com>
2025-02-26 17:42:00 +0800

    add get bios info demo

---
+++ BIOS/GetBIOSInfo/GetBIOSInfo.cpp
@@ -0,0 +1,36 @@
+﻿// GetBIOSInfo.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<Windows.h>
+
+typedef struct tagRawSMBIOSData
+{
+    BYTE    Used20CallingMethod;
+    BYTE    SMBIOSMajorVersion;
+    BYTE    SMBIOSMinorVersion;
+    BYTE    DmiRevision;
+    DWORD   Length;
+    BYTE    SMBIOSTableData[];
+}RawSMBIOSData,*PRawSMBIOSData;
+
+int main()
+{
+    UINT nSize = GetSystemFirmwareTable('ACPI', 0, NULL, 0);
+
+    if (nSize == 0)
+        return 0;
+
+    PRawSMBIOSData pRawBiosData = NULL;
+
+    BYTE* buf = new BYTE[nSize];
+
+
+    if (GetSystemFirmwareTable('RSMB', 0, buf, nSize) != nSize)
+        return 0;
+
+    pRawBiosData = (PRawSMBIOSData)buf;
+
+    delete[] buf;
+    return 0;
+}
