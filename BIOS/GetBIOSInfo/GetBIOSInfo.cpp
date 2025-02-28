2e45a751d520dc5c9344b71e748f0088886cef29
zhaotianff <zhaotianff@163.com>
2025-02-28 16:49:15 +0800

    update get bios info code

--- BIOS/GetBIOSInfo/GetBIOSInfo.cpp
+++ BIOS/GetBIOSInfo/GetBIOSInfo.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include<Windows.h>
+#include<map>
+#include<string>
 
 typedef struct tagRawSMBIOSData
 {
@@ -14,14 +16,60 @@ typedef struct tagRawSMBIOSData
     BYTE    SMBIOSTableData[];
 }RawSMBIOSData,*PRawSMBIOSData;
 
+typedef struct SMBIOSHeader {
+    BYTE    Type;         // 结构类型    
+    BYTE    Length;       // 该类型结构的格式化区域长度(请注意，长度取决于主板或系统支持的具体版本)    
+    WORD    Handle;       // 结构句柄(0～0xFEFF范围内的数字)
+}*PSMBIOSHeader;
+
+#pragma pack(1)
+// 系统信息(Type 1)SMBIOS结构的格式化区域的完整定义
+typedef struct _Type1SystemInformation
+{
+    SMBIOSHeader m_sHeader;       // SMBIOS结构头SMBIOSStructHeader
+    BYTE    m_bManufacturer;      // Manufacturer字符串的编号
+    BYTE    m_bProductName;       // Product Name字符串的编号
+    BYTE    m_bVersion;           // Version字符串的编号
+    BYTE    m_bSerialNumber;      // BIOS Serial Number字符串的编号
+    UUID    m_uuid;               // UUID
+    BYTE    m_bWakeupType;        // 标识导致系统启动的事件(原因)
+    BYTE    m_bSKUNumber;         // SKU Number字符串的编号
+    BYTE    m_bFamily;            // Family字符串的编号
+}Type1SystemInformation, * PType1SystemInformation;
+
+LPSTR FindStrFromSMBIOSDataStruct(PSMBIOSHeader pStructHeader, BYTE bNum)
+{
+    // 指向SMBIOS结构的未格式化区域(字符串数组)
+    LPBYTE lpByte = (LPBYTE)pStructHeader + pStructHeader->Length;
+
+    // 字符串编号从1开始
+    for (BYTE i = 1; i < bNum; i++)
+        lpByte += strlen((LPSTR)lpByte) + 1;
+
+    return (LPSTR)lpByte;
+}
+
+std::map<BYTE, std::string> wakeUpTypeMap = {
+        {0, "Reserved"},
+        {1, "Other"},
+        {2, "Unknown"},
+        {3, "APM Timer"},
+        {4, "Modem Ring"},
+        {5, "LAN Remote"},
+        {6, "Power Switch"},
+        {7, "PCI PME#"},
+        {8, "AC Power Restored"}
+};
+
 int main()
 {
-    UINT nSize = GetSystemFirmwareTable('ACPI', 0, NULL, 0);
+    UINT nSize = GetSystemFirmwareTable('RSMB', 0, NULL, 0);
 
     if (nSize == 0)
         return 0;
 
     PRawSMBIOSData pRawBiosData = NULL;
+    PSMBIOSHeader pStructHeader = NULL;
 
     BYTE* buf = new BYTE[nSize];
 
@@ -30,6 +78,49 @@ int main()
         return 0;
 
     pRawBiosData = (PRawSMBIOSData)buf;
+
+    //当前指针位置
+    auto lpData = pRawBiosData->SMBIOSTableData;
+
+    while ((lpData - pRawBiosData->SMBIOSTableData) < pRawBiosData->Length)
+    {
+        // 根据SMBIOS结构的格式化区域中的结构头的Type字段确定结构类型
+        // 确定结构类型以后再把pStructHeader转换为指向对应的格式化区域完整定义的指针
+        pStructHeader = (PSMBIOSHeader)lpData;
+
+        switch (pStructHeader->Type)
+        {
+        case 1: //Type 1
+        {
+            PType1SystemInformation pType1 = (PType1SystemInformation)pStructHeader;
+  
+            std::cout << "Manufacturer: " << FindStrFromSMBIOSDataStruct(pStructHeader, pType1->m_bManufacturer) << std::endl;
+            std::cout << "ProductName: " << FindStrFromSMBIOSDataStruct(pStructHeader, pType1->m_bProductName) << std::endl;
+            std::cout << "Version: " << FindStrFromSMBIOSDataStruct(pStructHeader, pType1->m_bVersion) << std::endl;
+            std::cout << "SerialNumber: " << FindStrFromSMBIOSDataStruct(pStructHeader, pType1->m_bSerialNumber) << std::endl;
+            std::cout << "SKUNumber: " << FindStrFromSMBIOSDataStruct(pStructHeader, pType1->m_bSKUNumber) << std::endl;
+            std::cout << "Family: " << FindStrFromSMBIOSDataStruct(pStructHeader, pType1->m_bFamily) << std::endl;
+            std::cout << "WakeupType: " << wakeUpTypeMap.find(pType1->m_bWakeupType)->second << std::endl;
+            
+        }
+            break;
+        default:
+            break;
+        }
+
+
+        // 遍历完格式化区域
+        lpData += pStructHeader->Length;
+
+        // 跳过未格式化区域
+        while ((*(LPWORD)lpData) != 0)
+        {
+            lpData++;
+        }
+
+        // 末尾 加2字节
+        lpData += 2;
+    }
 
     delete[] buf;
     return 0;
